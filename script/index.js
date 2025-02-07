97e277f3c8c193309df8f893073a8568ba6fe538
JI0 <alsn8539@gmail.com>
2025-02-07 17:57:38 +0900

    JS 추가 (2행 완료)

--- script/index.js
+++ script/index.js
@@ -1,7 +1,22 @@
-const mainSwiper = new Swiper('.swiper',{
-    /* effect:'fade',  */// 제자리에서 배너 이미지 변경하는 스와이퍼 효과
+const mainSwiper = new Swiper('.main_swiper',{
     autoplay:{
-        delay:4000, // 실제 배너 5~7초 많이 사용
+        delay:5000, // 실제 배너 5~7초 많이 사용
     },
     loop:true,
+    pagination:{
+        el:'.main_swiper .swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+        dynamicBullets:true,
+    },
+});
+
+const contentSwiper = new Swiper('.c_swiper',{
+    autoplay:{
+        delay:6000, // 실제 배너 5~7초 많이 사용
+    },
+    loop:true,
+    scrollbar: {
+        el: ".m_row .progress .swiper-scrollbar",
+    },
 });
